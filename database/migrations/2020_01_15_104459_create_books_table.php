18365c399aa96d908850b2c0930f8ab783d962f5
Kolossa Márton <kolossa.marton@utazom.com>
2020-01-15 14:01:17 +0100

    books table migration

---
+++ database/migrations/2020_01_15_104459_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->bigInteger('id');
+			$table->string('title');
+			$table->string('author');
+			$table->decimal('price');
+            $table->bigInteger('publisher_id', false, true);
+            $table->timestamps();
+			$table->primary('id');
+			$table->foreign('publisher_id')->references('id')->on('publishers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
