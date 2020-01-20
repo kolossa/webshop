77e76b0c1dbc2e64928f9c3b067f143de1b7ffd8
Kolossa Márton <kolosssa@gmail.com>
2020-01-20 23:02:23 +0100

    unit tests

--- app/Discount/DiscountRulePackage.php
+++ app/Discount/DiscountRulePackage.php
@@ -13,13 +13,26 @@ class DiscountRulePackage
      * $discount->amount1 in this case is the minimum amount of books of the specific publisher,
      * $discount->amount2 in this case is the number of cheapest book which will be free.
      *
-     * @param Book[] $books
+     * @param Book[] $books - books must be same publisher!
      * @param Discount $discount
      * @param $relatedDiscountsMap
      * @return int
      */
     public function getDiscountPrice(array $books, Discount $discount, \SplObjectStorage $relatedDiscountsMap)
     {
+        if(count($books)==0){
+            return 0;
+        }
+
+        $publisherCheck=[];
+        foreach ($books as $book){
+            $publisherCheck[$book->publisher_id]=$book->publisher_id;
+        }
+
+        if(count($publisherCheck)>1){
+            throw new \BadMethodCallException('Books must be assigned with the same publisher!');
+        }
+
         $discountPrice = 0;
         $numberOfDiscount = count($books) / $discount->amount1;
         $numberOfDiscount = floor($numberOfDiscount);
